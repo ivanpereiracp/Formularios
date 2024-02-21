d4a83b075ecfed4a730897e24da0399f582c2fc6
Ivan Pereira <kamaduvam@gmail.com>
2024-02-20 21:53:42 -0300

    Incluído outros objetos
    Incluído outros objetos

--- js/form.js
+++ js/form.js
@@ -9,9 +9,9 @@ document.querySelector("#submit").addEventListener("click", e => {
   const endereco = document.querySelector("#endereco").value;
   const fecha = document.querySelector("#fecha").value;
   const tipoconta = document.querySelector("#tipoconta").value;
-  const hora = document.querySelector("#hora").value;
-  const empleado = document.querySelector("#empleado").value;
-  const servicio = document.querySelector("#servicio").value;
+  //const hora = document.querySelector("#hora").value;
+  //const empleado = document.querySelector("#empleado").value;
+  //const servicio = document.querySelector("#servicio").value;
   const resp = document.querySelector("#respuesta");
 
   resp.classList.remove("fail");
@@ -29,13 +29,13 @@ document.querySelector("#submit").addEventListener("click", e => {
 		*_Endereço_*%0A
 		${endereco}%0A
 		*_Tipo de conta_*%0A
-		${tipoconta}%0A		
-		*Indica la hora de tu reserva*%0A
-		${hora}%0A
-		*Empleado de preferencia*%0A
-		${empleado}%0A
-		*¿Cuál es el servicio que se desea realizar?*%0A
-		${servicio}`;
+		${tipoconta}%0A `;		
+		//*Indica la hora de tu reserva*%0A
+		//${hora}%0A
+		//*Empleado de preferencia*%0A
+		//${empleado}%0A
+		//*¿Cuál es el servicio que se desea realizar?*%0A
+		//${servicio}`;
 
   if (cliente === "" || fecha === "" || titulo === "" || endereco === "") {
     resp.classList.add("fail");
